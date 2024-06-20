4402dfd2cf2e381daa5d0ea1e37326b777176272
Dionyysius <109593508+Dionyysius@users.noreply.github.com>
2024-06-20 22:30:12 +0700

    FistCommit

---
+++ resources/views/themes/indotoko/home.blade.php
@@ -0,0 +1,256 @@
+@extends('themes.indotoko.layouts.app')
+@include('themes.indotoko.shared.slider')
+@section('content')
+
+<!-- Popular -->
+<section class="popular">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-6">
+        <h1>Popular</h1>
+      </div>
+      <div class="col-6 text-end">
+        <a href="#" class="btn-first">View All</a>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-lg-3 col-6">
+        <div class="card card-body p-lg-4 p3">
+          <a href="#"><img src="img/SepedaBMXUkr20.jpg" alt="" class="img-fluid rounded"></a>
+          <h3 class="product-name mt-3">Sepeda BMX</h3>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+          </div>
+          <div class="detail d-flex justify-content-between align-items-center mt-4">
+              <p class="price">IDR 950.000</p>
+              <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-6">
+        <div class="card card-body p-lg-4 p3">
+          <a href="#"><img src="img/Sepeda ctb Phonenix.jpg" alt="" class="img-fluid rounded"></a>
+          <h3 class="product-name mt-3">Sepeda CTB Ukr 26</h3>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+          </div>
+          <div class="detail d-flex justify-content-between align-items-center mt-4">
+              <p class="price">IDR 2.500.000</p>
+              <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-6 mt-3 mt-lg-0">
+        <div class="card card-body p-lg-4 p3">
+          <a href="#"><img src="img/Baby Walker.jpg" alt="" class="img-fluid rounded"></a>
+          <h3 class="product-name mt-3">Baby Walker</h3>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+          </div>
+          <div class="detail d-flex justify-content-between align-items-center mt-4">
+              <p class="price">IDR 700.000</p>
+              <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-6 mt-3 mt-lg-0">
+        <div class="card card-body p-lg-4 p3">
+          <a href="#"><img src="img/Sepeda BMX Ukr 12.jpg" alt="" class="img-fluid rounded"></a>
+          <h3 class="product-name mt-3">Sepeda BMX Ukuran 12</h3>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+          </div>
+          <div class="detail d-flex justify-content-between align-items-center mt-4">
+              <p class="price">IDR 950.000</p>
+              <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Latest -->
+<section class="latest">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-6">
+        <h1>Latest</h1>
+      </div>
+      <div class="col-6 text-end">
+        <a href="#" class="btn-first">View All</a>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-lg-3 col-6">
+        <div class="card card-body p-lg-4 p3">
+          <a href="#"><img src="img/SepedaBMXUkr20.jpg" alt="" class="img-fluid rounded"></a>
+          <h3 class="product-name mt-3">Sepeda BMX Ukuran 20</h3>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+          </div>
+          <div class="detail d-flex justify-content-between align-items-center mt-4">
+              <p class="price">IDR 950.000</p>
+              <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-6">
+        <div class="card card-body p-lg-4 p3">
+          <a href="#"><img src="img/Sepeda BMX Ukr 12.jpg" alt="" class="img-fluid rounded"></a>
+          <h3 class="product-name mt-3">Sepeda BMX Ukuran 12</h3>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+          </div>
+          <div class="detail d-flex justify-content-between align-items-center mt-4">
+              <p class="price">IDR 850.000</p>
+              <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-6 mt-3 mt-lg-0">
+        <div class="card card-body p-lg-4 p3">
+          <a href="#"><img src="img/Sepeda Lipat Ukr 20.jpeg" alt="" class="img-fluid rounded"></a>
+          <h3 class="product-name mt-3">Sepeda Lipat Ukuran 20</h3>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+          </div>
+          <div class="detail d-flex justify-content-between align-items-center mt-4">
+              <p class="price">IDR 1.950.000</p>
+              <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-6 mt-3 mt-lg-0">
+        <div class="card card-body p-lg-4 p3">
+          <a href="#"><img src="img/Sepeda ukr 26.png" alt="" class="img-fluid rounded"></a>
+          <h3 class="product-name mt-3">Sepeda Ukuran 26</h3>
+          <div class="rating">
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+            <i class="bx bxs-star"></i>
+          </div>
+          <div class="detail d-flex justify-content-between align-items-center mt-4">
+              <p class="price">IDR 2.450.000</p>
+              <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+          </div>
+        </div>
+      </div>
+<div class="col-lg-3 col-6 mt-3 mt-lg-0">
+  <div class="card card-body p-lg-4 p3">
+    <a href="#"><img src="img/Baby Walker.jpg" alt="" class="img-fluid rounded"></a>
+    <h3 class="product-name mt-3">Baby Walker</h3>
+    <div class="rating">
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+    </div>
+    <div class="detail d-flex justify-content-between align-items-center mt-4">
+        <p class="price">IDR 700.000</p>
+        <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+    </div>
+  </div>
+      </div>
+<div class="col-lg-3 col-6 mt-3 mt-lg-0">
+  <div class="card card-body p-lg-4 p3">
+    <a href="#"><img src="img/Sepedaa bayi.jpg" alt="" class="img-fluid rounded"></a>
+    <h3 class="product-name mt-3">Sepeda Bayi</h3>
+    <div class="rating">
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+    </div>
+    <div class="detail d-flex justify-content-between align-items-center mt-4">
+        <p class="price">IDR 500.000</p>
+        <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+    </div>
+  </div>
+      </div>
+<div class="col-lg-3 col-6 mt-3 mt-lg-0">
+  <div class="card card-body p-lg-4 p3">
+    <a href="#"><img src="img/Sepeda ctb Phonenix.jpg" alt="" class="img-fluid rounded"></a>
+    <h3 class="product-name mt-3">Sepeda CTB</h3>
+    <div class="rating">
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+    </div>
+    <div class="detail d-flex justify-content-between align-items-center mt-4">
+        <p class="price">IDR 2.500.000</p>
+        <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+    </div>
+  </div>
+      </div>
+<div class="col-lg-3 col-6 mt-3 mt-lg-0">
+  <div class="card card-body p-lg-4 p3">
+    <a href="#"><img src="img/Sepeda mini Phoenix.jpg" alt="" class="img-fluid rounded"></a>
+    <h3 class="product-name mt-3">Sepeda mini Phoenix</h3>
+    <div class="rating">
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+      <i class="bx bxs-star"></i>
+    </div>
+    <div class="detail d-flex justify-content-between align-items-center mt-4">
+        <p class="price">IDR 2.900.000</p>
+        <a href="#" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
+    </div>
+  </div>
+      </div>
+
+<!-- Subscribe  -->
+<section class="subscribe">
+  <div class="container">
+    <div class="subscribe-wrapper">
+      <div class="row justify-content-center text-center">
+        <div class="col-lg-6 col-md-7 col-10 col-sub">
+          <h1>Subscribe to get latest updates!</h1>
+          <form action="#" class="mt-5">
+            <div class="input-group w-100">
+              <input type="email" class="form-control" placeholder="Type your email ..">
+              <button class="btn btn-outline-warning">Subscribe</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+@endsection
